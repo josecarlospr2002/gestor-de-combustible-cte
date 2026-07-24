be21ee52bd16226779435a904d3a2df9179aa459
josecarlospr2002 <josecarlosop23miami@gmail.com>
2026-07-24 11:50:21 -0700

    Creada vista de vehículos con tabla estilizada y acceso restringido por rol

--- combustible/urls.py
+++ combustible/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
+    #path('solicitud/crear/', views.crear_solicitud, name='crear_solicitud'),
+    path('transporte/', views.lista_transporte, name='lista_transporte'),
 ]
--- combustible/views.py
+++ combustible/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import SolicitudCombustible, Transporte
 
 
 @login_required
 def dashboard(request):
     return render(request, 'combustible/dashboard.html')
+
+@login_required
+def lista_transporte(request):
+    vehiculos = Transporte.objects.all()
+    return render(request, 'combustible/lista_transporte.html', {'vehiculos': vehiculos})
